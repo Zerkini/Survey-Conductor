08aa3198078a657f51872bcb8ac2ce4527b29fea
jcub731 <ksolawa@wp.pl>
2018-11-27 16:44:14 +0100

    initial commit - spring

--- src/main/java/com/psychosurvey/SurveyApplication.java
+++
@@ -1,7 +0,0 @@
-package com.psychosurvey;
-
-public class SurveyApplication {
-    public static void main(String[] args) {
-        System.out.println("Survey Application");
-    }
-}
---
+++ src/main/java/com/psychosurvey/psychosurvey/PsychosurveyApplication.java
@@ -0,0 +1,12 @@
+package com.psychosurvey.psychosurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychosurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsychosurveyApplication.class, args);
+	}
+}
